3acd5d139db66d06d495a12de08e3005a2665432
Pierre-Henri Trivier <phtrivier@sierrawireless.com>
2015-03-09 18:56:27 +0100

    jenkins build test

---
+++ release/deploy.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+####### Constants
+
+VERSION=dev-`date +%Y-%m-%d-%s`
+ENV_NAME=dev
+NETWORK_STACK=$ENV_NAME-net
+STACK_NAME=$ENV_NAME-av-sched
+
+####### Functions
+
+function build_java {
+    echo
+    echo ">>"
+    echo ">> Build java"
+    echo ">>"
+    echo
+
+    mvn clean package -q
+}
+
+function upload_resources {
+    echo
+    echo ">>"
+    echo ">> Upload resources to S3"
+    echo ">>"
+    echo
+
+    aws s3 cp av-sched/target/av-sched-*-exec.jar s3://av-repo/apps/av-sched/${VERSION}/artifacts/av-sched.jar
+    aws s3 cp release/cloudformation/av-sched.template s3://av-repo/deployments/av-sched/${VERSION}/cloudformation/
+}
+
+# function extract_network_params {
+#     network_params=`aws cloudformation describe-stacks --stack-name $NETWORK_STACK`
+#     if [ "$network_params" ]
+#     then
+#         network_params=`echo $network_params | jq -c '.Stacks[0].Parameters \
+#             | map(select( \
+#                 .ParameterKey == "NetworkPrefix" or \
+#                 .ParameterKey == "EnvName" ))'`
+#     else
+#         echo "Network stack not found: EXIT"
+#         exit 1
+#     fi
+# }
+
+# function extract_network_resources {
+#     network_resources=`aws cloudformation describe-stack-resources --stack-name $NETWORK_STACK`
+#     if [ "$network_resources" ]
+#     then
+#         network_resources=`echo $network_resources | jq -c '.StackResources \
+#             | map({ParameterKey: .LogicalResourceId, ParameterValue: .PhysicalResourceId}) \
+#             | map(select( \
+#                 .ParameterKey == "Vpc" or \
+#                 .ParameterKey == "PrivateSubnetA" or \
+#                 .ParameterKey == "PrivateSubnetB" or \
+#                 .ParameterKey == "PrivateSubnetC" or \
+#                 .ParameterKey == "PublicRouteTable" or \
+#                 .ParameterKey == "PrivateSecurityGroup" ))'`
+#     else
+#         echo "Network stack not found: EXIT"
+#         exit 1
+#     fi
+# }
+
+function build_stack_params {
+    aa=`echo  $NETWORK_STACK`
+    extract=`noho stack_extract \
+        -stackName=$NETWORK_STACK \
+        -parameterNames=EnvName,EnvType,NetworkPrefix \
+        -resourceNames=Vpc,PrivateSubnetA,PrivateSubnetB,PrivateSubnetC,PrivateSecurityGroup,PublicWithVpnRouteTable,AlarmSNSTopic,S3LoggingBucket`
+    stack_params=`echo $extract | noho append_parameters \
+        -parameters=AppVersion=$VERSION`
+    stack_params=`echo [$stack_params,$(cat release/conf.json)] | jq -c '.[0] + .[1]'`
+
+    echo "New stack paremeters:"
+    echo $stack_params | jq '.'
+}
+
+function create_or_update_stack {
+    local stack_exists=`aws cloudformation list-stacks --stack-status-filter \
+                        CREATE_COMPLETE CREATE_IN_PROGRESS UPDATE_IN_PROGRESS UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE \
+                        | grep "StackName" | grep $STACK_NAME`
+    if [ -z "$stack_exists" ]
+    then
+        echo
+        echo ">"
+        echo "> Creating stack"
+        echo ">"
+        echo
+
+        aws cloudformation create-stack \
+            --template-url https://s3-eu-west-1.amazonaws.com/av-repo/deployments/av-sched/${VERSION}/cloudformation/av-sched.template \
+            --stack-name $STACK_NAME \
+            --parameters $stack_params \
+            --stack-policy-url https://s3-eu-west-1.amazonaws.com/av-repo/stack-update-policies/deny_all.json \
+            --capabilities CAPABILITY_IAM | jq '.'
+    else
+        echo
+        echo ">"
+        echo "> Updating stack"
+        echo ">"
+        echo
+
+        aws cloudformation update-stack \
+            --template-url https://s3-eu-west-1.amazonaws.com/av-repo/deployments/av-sched/${VERSION}/cloudformation/av-sched.template \
+            --stack-name $STACK_NAME \
+            --parameters $stack_params \
+            --stack-policy-url https://s3-eu-west-1.amazonaws.com/av-repo/stack-update-policies/allow_all.json \
+            --capabilities CAPABILITY_IAM  | jq '.'
+    fi
+}
+
+####### Main
+
+echo ENV_NAME=$ENV_NAME
+echo NETWORK_STACK=$NETWORK_STACK
+echo VERSION=$VERSION
+echo STACK_NAME=$STACK_NAME
+
+
+if [ $? -eq 0 ]
+    then build_java
+fi
+if [ $? -eq 0 ]
+    then upload_resources
+fi
+if [ $? -eq 0 ]
+    then build_stack_params
+fi
+if [ $? -eq 0 ]
+    then create_or_update_stack
+fi
+
