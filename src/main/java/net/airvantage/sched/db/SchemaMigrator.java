5578969206688885b451380b9a159fdcbad52ceb
Pierre-Henri Trivier <phtrivier@sierrawireless.com>
2015-03-16 16:57:37 +0100

    Adding autocommit option

--- src/main/java/net/airvantage/sched/db/SchemaMigrator.java
+++ src/main/java/net/airvantage/sched/db/SchemaMigrator.java
@@ -10,9 +10,14 @@ import org.apache.ibatis.migration.FileMigrationLoader;
 import org.apache.ibatis.migration.JdbcConnectionProvider;
 import org.apache.ibatis.migration.operations.BootstrapOperation;
 import org.apache.ibatis.migration.operations.UpOperation;
+import org.apache.ibatis.migration.options.DatabaseOperationOption;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SchemaMigrator {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SchemaMigrator.class);
+    
     private ConnectionProvider connectionProvider;
     private FileMigrationLoader fileMigrationLoader;
 
@@ -28,13 +33,17 @@ public class SchemaMigrator {
     }
 
     public void bootstrap() {
-        new BootstrapOperation(true).operate(connectionProvider, fileMigrationLoader, null, null);
+        DatabaseOperationOption options = new DatabaseOperationOption();
+        options.setAutoCommit(true);
+        new BootstrapOperation(true).operate(connectionProvider, fileMigrationLoader, options, System.out);
     }
 
     public void migrate() {
-        new LockOperation().operate(connectionProvider, fileMigrationLoader, null, null);
-        new UpOperation().operate(connectionProvider, fileMigrationLoader, null, null);
-        new UnlockOperation().operate(connectionProvider, fileMigrationLoader, null, null);
+        DatabaseOperationOption options = new DatabaseOperationOption();
+        options.setAutoCommit(true);
+        new LockOperation().operate(connectionProvider, fileMigrationLoader, options, System.out);
+        new UpOperation().operate(connectionProvider, fileMigrationLoader, options, System.out);
+        new UnlockOperation().operate(connectionProvider, fileMigrationLoader, options, System.out);
     }
 
 }
