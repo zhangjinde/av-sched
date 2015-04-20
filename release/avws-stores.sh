026d4c92ebdc7a4a49a76122ce779295166799c8
Jerome C <jcombes@sierrawireless.com>
2015-04-20 16:25:15 +0200

    New script to build, release and deploy av-sched

--- release/avws-stores.sh
+++ release/avws-stores.sh
@@ -5,10 +5,6 @@ SCRIPT_DIR=`dirname $(realpath $0)`
 # Name of the repo where to upload the resources (artifact, template, ...)
 REPO_NAME=av-sched-stores
 
-# Default values for EnvName
-declare -A DEFAULT_PARAMS
-DEFAULT_PARAMS[EnvName]=dev
-
 # Configuration files located on AWS S3 buckets
 # The name of the configuration to be used is defined by the argument '--aws-cfg <ConfigName>'
 # AWS_S3_BUCKETS[ConfigName]="s3://...""
@@ -27,33 +23,48 @@ SECURED_STACK_PARAMS_NAMES=( DBPwd )
 
 # ----------------------------------------------------------------------------------------
 # 
-# Initialize the variables used by the script.
-# At least the variables ENV_NAME VERSION NETWORK_STACK STACK_NAME need to be set.
-# 
-# ----------------------------------------------------------------------------------------
-
-function init_variables {
-    ENV_NAME=$(get_parameter EnvName)
-    NETWORK_STACK=$ENV_NAME-net
-    STACK_NAME=$ENV_NAME-av-sched-stores
-    
-    # Get the parameter 'Version'. If the parameter is not defined then a value will be generated.
-    VERSION=$(get_parameter Version $ENV_NAME-`date -u +%Y%m%d%H%M%S`)
-}
-
-# ----------------------------------------------------------------------------------------
-# 
 # Build all required artifacts for your application and upload them on S3.
-# This function is only called if the command line parameter '-build' is used.
+# This function is called when the command 'build' is used.
 # 
 # ----------------------------------------------------------------------------------------
 
 function build {
 
+    print "No artifact to build" 6
+}
+
+
+# ----------------------------------------------------------------------------------------
+# 
+# Upload the artifacts to S3.
+# The variable 'VERSION' contains the version of the application to release.
+# This function is called when the command 'release' is used.
+# 
+# ----------------------------------------------------------------------------------------
+
+function release {
+
     print "Upload resources to S3" 6
 
     print "Uploading cloudformation" 2
     upload_template $SCRIPT_DIR/cloudformation/av-sched-stores.template
+}
+
+
+# ----------------------------------------------------------------------------------------
+# 
+# Initialize the required variables to be used to deploy the stack.
+# The following variables need to be set: ENV_NAME NETWORK_STACK STACK_NAME.
+# The variable 'VERSION' contains the version of the application to deploy.
+# This function is called when the command 'deploy' is used.
+# 
+# ----------------------------------------------------------------------------------------
+
+function prepare_deploy {
+    # Read the parameter EnvName
+    ENV_NAME=$(get_parameter EnvName)
+    NETWORK_STACK=$ENV_NAME-net
+    STACK_NAME=$ENV_NAME-av-sched-stores
 }
 
 
@@ -63,5 +74,5 @@ function build {
 ##
 ## ==================================================================================
 
-aws s3 cp s3://av-repo/tools/deploy-tools.sh $SCRIPT_DIR
-source $SCRIPT_DIR/deploy-tools.sh
+aws s3 cp s3://av-repo/tools/avws-tools.sh $SCRIPT_DIR
+source $SCRIPT_DIR/avws-tools.sh
--- release/avws.sh
+++ release/avws.sh
@@ -5,10 +5,6 @@ SCRIPT_DIR=`dirname $(realpath $0)`
 # Name of the repo where to upload the resources (artifact, template, ...)
 REPO_NAME=av-sched
 
-# Default values for EnvName
-declare -A DEFAULT_PARAMS
-DEFAULT_PARAMS[EnvName]=dev
-
 # Configuration files located on AWS S3 buckets
 # The name of the configuration to be used is defined by the argument '--aws-cfg <ConfigName>'
 # AWS_S3_BUCKETS[ConfigName]="s3://...""
@@ -26,29 +22,10 @@ SECURED_STACK_PARAMS_NAMES=( DBPwd AvSchedSecret )
 
 
 # ----------------------------------------------------------------------------------------
-#
-# Initialize the variables used by the script.
-# At least the variables ENV_NAME VERSION NETWORK_STACK STACK_NAME need to be set.
-#
-# ----------------------------------------------------------------------------------------
-
-function init_variables {
-    ENV_NAME=$(get_parameter EnvName)
-    NETWORK_STACK=$ENV_NAME-net
-    STACK_NAME=$ENV_NAME-av-sched
-
-    # Get the parameter 'Version'. If the parameter is not defined then a value will be generated.
-    VERSION=$(get_parameter Version $ENV_NAME-`date -u +%Y%m%d%H%M%S`)
-    # Set the parameter 'Version' to be sure the generated value is taken into account.
-    set_parameter Version $VERSION
-}
-
-# ----------------------------------------------------------------------------------------
-#
+# 
 # Build all required artifacts for your application and upload them on S3.
-#
-# This function is only called if the command line parameter '-build' is used.
-#
+# This function is called when the command 'build' is used.
+# 
 # ----------------------------------------------------------------------------------------
 
 function build {
@@ -63,16 +40,46 @@ function build {
 
     # Back to the initial directory
     cd $initial_dir
+}
 
-    # --------
+
+# ----------------------------------------------------------------------------------------
+# 
+# Upload the artifacts to S3.
+# The variable 'VERSION' contains the version of the application to release.
+# This function is called when the command 'release' is used.
+# 
+# ----------------------------------------------------------------------------------------
+
+function release {
 
     print "Upload resources to S3" 6
 
+    print "Uploading artifact" 2
+    upload_to_repo $SCRIPT_DIR/../target/av-sched-*-exec.jar av-sched.jar
+
     print "Uploading cloudformation" 2
     upload_template $SCRIPT_DIR/cloudformation/av-sched.template
+}
 
-    print "Uploading artifact" 2
-    upload_on_repo $SCRIPT_DIR/../target/av-sched-*-exec.jar av-sched.jar
+
+# ----------------------------------------------------------------------------------------
+# 
+# Initialize the required variables to be used to deploy the stack.
+# The following variables need to be set: ENV_NAME NETWORK_STACK STACK_NAME.
+# The variable 'VERSION' contains the version of the application to deploy.
+# This function is called when the command 'deploy' is used.
+# 
+# ----------------------------------------------------------------------------------------
+
+function prepare_deploy {
+    # Read the parameter EnvName
+    ENV_NAME=$(get_parameter EnvName)
+    NETWORK_STACK=$ENV_NAME-net
+    STACK_NAME=$ENV_NAME-av-sched
+
+    # Set the parameter 'Version' as a stack parameter
+    set_parameter Version $VERSION
 }
 
 
@@ -82,5 +89,5 @@ function build {
 ##
 ## ==================================================================================
 
-aws s3 cp s3://av-repo/tools/deploy-tools.sh $SCRIPT_DIR
-source $SCRIPT_DIR/deploy-tools.sh
+aws s3 cp s3://av-repo/tools/avws-tools.sh $SCRIPT_DIR
+source $SCRIPT_DIR/avws-tools.sh
